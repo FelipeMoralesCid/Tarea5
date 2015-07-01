bcc13d540cfeac426d63475303ebcbff3c2d2018
FelipeMoralesCid <85283102@yopmail.com>
2015-06-30 23:46:51 -0400

    Update .c

--- .c
+++ .c
@@ -1,22 +1,32 @@
+//Alumno : Felipe Morales Cid
+//RUN : 18740772-9
+//TAREA N5
+
+
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
 
-  
+  //aqui van todos los comandos basicos que voy a requerir//
   int largo,cont,pot=0,pan,i;
   char palabra[30],control[]="A";
   printf("\nescriba la palabra que desee para avanzar\n");
   gets(palabra);
   largo=strlen(palabra);
   strupr(palabra);
-
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  // esto es para darle al ascii valores mas manejables con a en 0,b en 1 etc//
 for (cont = 0; cont < largo+1; ++cont)
 {
   palabra[cont]=palabra[cont]-control[0];
 
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// se le da el correcto valor con el cual iniciar mediante esta forma//
 if (palabra[0]>13)
 {
   i=(palabra[0]-13)*2;
@@ -27,8 +37,9 @@ else{
 pot=palabra[0];
 
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+// con los siguientes if se verifican todas las rutas para ver cual es la mas conveniente a seguir
 for (cont = 0; cont < largo-1; ++cont)
 {
     pan=0;
@@ -60,6 +71,8 @@ for (cont = 0; cont < largo-1; ++cont)
 
 
 }
+
+////////////////////////////////////////////////////////////////////////
 printf("El total recorrido es de %d espacios.\n",pot);
 
 
